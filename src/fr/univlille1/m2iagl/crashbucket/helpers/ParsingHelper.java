c6e4d0a3e8402b0eab53cabc246b100430cfc160
rbadr <rahal.badr@gmail.com>
2016-10-25 17:16:13 +0200

    Adds the Random stacktrace to bucket assignment

--- src/fr/univlille1/m2iagl/crashbucket/helpers/ParsingHelper.java
+++ src/fr/univlille1/m2iagl/crashbucket/helpers/ParsingHelper.java
@@ -1,6 +1,8 @@
 package fr.univlille1.m2iagl.crashbucket.helpers;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ParsingHelper {
         public static void listFilesForFolder(final File folder) {
@@ -9,10 +11,41 @@ public class ParsingHelper {
                     listFilesForFolder(fileEntry);
                 } 
                 else {
-                    System.out.println(fileEntry.getParent());
+                    //System.out.println(fileEntry.getParent());
                     System.out.println(fileEntry.getParentFile().getParentFile().getName());
-                    System.out.println(fileEntry.getName());
+                    //System.out.println(fileEntry.getName());
                 }
             }
         }   
+        
+        public static List<String> listTrainingFolder(final File folder) {
+            List<String> res = new ArrayList();
+            for (final File fileEntry : folder.listFiles()) {
+                if (fileEntry.isDirectory()) {
+                    res.addAll(listTrainingFolder(fileEntry));
+                } 
+                else {
+                    //System.out.println(fileEntry.getParent());
+                    res.add(fileEntry.getParentFile().getParentFile().getName());
+                    //System.out.println(fileEntry.getName());
+                }
+            }
+          return res;
+        }
+        
+        public static List<String> listTestingFolder(final File folder) {
+            List<String> res = new ArrayList();
+            for (final File fileEntry : folder.listFiles()) {
+                if (fileEntry.isDirectory()) {
+                    //listTestingFolder(fileEntry);
+                    res.addAll(listTestingFolder(fileEntry));
+                } 
+                else {
+                    //System.out.println(fileEntry.getParent());
+                    res.add(fileEntry.getName().substring(0,fileEntry.getName().indexOf(".txt")));
+                    //System.out.println(fileEntry.getName());
+                }
+            }
+          return res;
+        }
 }
--- src/fr/univlille1/m2iagl/crashbucket/main/Main.java
+++ src/fr/univlille1/m2iagl/crashbucket/main/Main.java
@@ -1,9 +1,12 @@
 package fr.univlille1.m2iagl.crashbucket.main;
 
 import static fr.univlille1.m2iagl.crashbucket.helpers.ParsingHelper.listFilesForFolder;
+import static fr.univlille1.m2iagl.crashbucket.helpers.ParsingHelper.listTestingFolder;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
+import static fr.univlille1.m2iagl.crashbucket.helpers.ParsingHelper.listTrainingFolder;
+import java.util.Random;
 
 public class Main {
 
@@ -14,8 +17,21 @@ public class Main {
     public static List<File> testingRessources = new ArrayList<File>();
         
     public static void main(String[] args) {
-        final File folder = new File(NAUTILUS_TRAINING_PATH);
-        listFilesForFolder(folder);
+        final File trainingFolder = new File(NAUTILUS_TRAINING_PATH);
+        final File testingFolder = new File(NAUTILUS_TESTING_PATH);
+        
+        List<String> bucketChoice = listTrainingFolder(trainingFolder);
+        List<String> stackTraces = listTestingFolder(testingFolder);
+        
+        Random randomGenerator = new Random();
+        
+        for(String stackTrace : stackTraces){
+            int index = randomGenerator.nextInt(bucketChoice.size());
+            String item = bucketChoice.get(index);
+            System.out.println(stackTrace+" -> " + item);
+        }
+        
+        
         
         //TODO
     }
