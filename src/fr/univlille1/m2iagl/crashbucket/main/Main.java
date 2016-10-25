9df6e5bde39d89b9385b7c17f900f6aa40951b2f
rbadr <rahal.badr@gmail.com>
2016-10-25 17:30:46 +0200

    Used Diamond Inference

--- src/fr/univlille1/m2iagl/crashbucket/main/Main.java
+++ src/fr/univlille1/m2iagl/crashbucket/main/Main.java
@@ -1,6 +1,5 @@
 package fr.univlille1.m2iagl.crashbucket.main;
 
-import static fr.univlille1.m2iagl.crashbucket.helpers.ParsingHelper.listFilesForFolder;
 import static fr.univlille1.m2iagl.crashbucket.helpers.ParsingHelper.listTestingFolder;
 import java.io.File;
 import java.util.ArrayList;
@@ -13,8 +12,8 @@ public class Main {
     public static final String NAUTILUS_TRAINING_PATH = "D:\\IAGL\\OPL\\nautilus\\nautilus-training";
     public static final String NAUTILUS_TESTING_PATH = "D:\\IAGL\\OPL\\nautilus\\nautilus-testing";
     
-    public static List<File> trainingRessources = new ArrayList<File>();
-    public static List<File> testingRessources = new ArrayList<File>();
+    public static List<File> trainingRessources = new ArrayList<>();
+    public static List<File> testingRessources = new ArrayList<>();
         
     public static void main(String[] args) {
         final File trainingFolder = new File(NAUTILUS_TRAINING_PATH);
@@ -28,7 +27,7 @@ public class Main {
         for(String stackTrace : stackTraces){
             int index = randomGenerator.nextInt(bucketChoice.size());
             String item = bucketChoice.get(index);
-            System.out.println(stackTrace+" -> " + item);
+            System.out.println(stackTrace+"  -> " + item);
         }
         
         
