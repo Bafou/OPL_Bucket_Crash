e906108626c2fd067d7019d052d833780b64f760
rbadr <rahal.badr@gmail.com>
2016-11-07 22:01:28 +0100

    Adds the output writer + refacto

--- src/fr/univlille1/m2iagl/crashbucket/main/RandomAssignment.java
+++ src/fr/univlille1/m2iagl/crashbucket/main/RandomAssignment.java
@@ -1,5 +1,7 @@
 package fr.univlille1.m2iagl.crashbucket.main;
 
+import fr.univlille1.m2iagl.crashbucket.constant.Paths;
+import static fr.univlille1.m2iagl.crashbucket.helpers.OutputWriter.generateOutputFile;
 import static fr.univlille1.m2iagl.crashbucket.helpers.ParsingHelper.listTestingFolder;
 import java.io.File;
 import java.util.ArrayList;
@@ -7,17 +9,14 @@ import java.util.List;
 import static fr.univlille1.m2iagl.crashbucket.helpers.ParsingHelper.listTrainingFolder;
 import java.util.Random;
 
-public class Main {
-
-    public static final String NAUTILUS_TRAINING_PATH = "D:\\IAGL\\OPL\\nautilus\\nautilus-training";
-    public static final String NAUTILUS_TESTING_PATH = "D:\\IAGL\\OPL\\nautilus\\nautilus-testing";
+public class RandomAssignment {
     
     public static List<File> trainingRessources = new ArrayList<>();
     public static List<File> testingRessources = new ArrayList<>();
         
     public static void main(String[] args) {
-        final File trainingFolder = new File(NAUTILUS_TRAINING_PATH);
-        final File testingFolder = new File(NAUTILUS_TESTING_PATH);
+        final File trainingFolder = new File(Paths.NAUTILUS_TRAINING);
+        final File testingFolder = new File(Paths.NAUTILUS_TESTING);
         
         List<String> bucketChoice = listTrainingFolder(trainingFolder);
         List<String> stackTraces = listTestingFolder(testingFolder);
@@ -27,12 +26,8 @@ public class Main {
         for(String stackTrace : stackTraces){
             int index = randomGenerator.nextInt(bucketChoice.size());
             String item = bucketChoice.get(index);
-            System.out.println(stackTrace+"  -> " + item);
+            generateOutputFile(stackTrace,item,"RandomAssignementOutput",Paths.NAUTILUS);
         }
-        
-        
-        
-        //TODO
     }
     
 }
